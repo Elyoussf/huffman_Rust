13aea426143aa281b56ef48fb9f8f67b37819396
Elyoussf <strangeamazigh1968@gmail.com>
2024-08-25 19:52:01 +0100

    just relax it's me

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::collections::BinaryHeap;
-use colored::*;
 use std::collections::HashMap;
+use colored::*;
 
-struct Tree{
-score : u64,
-label : String,
-left : Option<Box<Tree>>,
-right : Option<Box<Tree>>,
+struct Tree {
+    score: u64,
+    label: Option<char>,  // Use Option<char> to handle leaf and non-leaf nodes
+    left: Option<Box<Tree>>,
+    right: Option<Box<Tree>>,
 }
+
 impl Ord for Tree {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        // Reverse the order to prioritize lower scores
         other.score.cmp(&self.score)
     }
 }
@@ -29,99 +29,71 @@ impl PartialEq for Tree {
 
 impl Eq for Tree {}
 
-fn frequencies(payload : String,mut memo : HashMap<char, u64>) -> HashMap<char,u64>{
-for chara in payload.chars(){
-*memo.entry(chara).or_insert(0)+=1;
-}
-return memo ;
-}
-fn main(){
-let text = "Huffman coding is a lossless data compression algorithm. It is used to compress data in a way that reduces the overall size of the data without losing any information.
-Huffman coding is a lossless data compression algorithm. It is used to compress data in a way that reduces the overall size of the data without losing any information.
-vjhfdjhvjfhjhjxn,nbxjcjxhcjhxjcdhcjkkmldszaudehf x,dxdbd  dbcjhdbhddncjbd cnb dbjhfindcn nd nbjhrhfj nc  vkjbejvbrekenkrejbvkj vn rjvjrhfoijzsnxsqnxlkjsqlkdjzeoifpofibejbvkndclkdnk Huffman coding is a lossless data compression algorithm. It is used to compress data in a way that reduces the overall size of the data without losing any information.";
-let payload  = String::from(text);
-let memo :HashMap<char, u64> = HashMap::new();    
-let freq = frequencies(payload, memo);
-let mut min_heap : BinaryHeap<Tree> = BinaryHeap::new();
-//add all elements to the current BinaryHeap
-for (u,v) in &freq{
-let current_Tree = Tree{
-score : *v,
-label : String::from(*u),
-left : None,
-right : None
-};
-
-min_heap.push(current_Tree)
-}
-// at this point we are ready to start the algo 
-// while we have more than one element >=2 in BinaryHeap 
-// we pop two we addup their score 
-// create a new parent node with the concatenation of their label and has this just calculated
-// score 
-// add it to BinaryHeap
-while min_heap.len()>=2{
-let node1 = min_heap.pop().unwrap();
-let node2 = min_heap.pop().unwrap();
-let newScore = node2.score+node1.score;
-let mut newLabel = node1.label.clone();
-let oldLabel = node2.label.as_str();
-newLabel.push_str(oldLabel);
-let newNode = Tree{
-score : newScore,
-label : newLabel,
-left : Some(Box::new(node1)),
-right : Some(Box::new(node2)) 
-};
-
-min_heap.push(newNode);
-}
-let head = min_heap.pop().unwrap();
-
-let mut codes : HashMap<String,String> = HashMap::new();
-
-let code = String::new(); 
-findCharacter(head, &mut codes, code);
-let mut size_original = 0;
-let mut size_compressed = 0;
-
-for (u,v) in codes{
-size_compressed += (v.len() as u64)*freq[&u.chars().next().unwrap()];
-size_original+=freq[&u.chars().next().unwrap()]*8;
-
+fn frequencies(payload: &str) -> HashMap<char, u64> {
+    let mut memo: HashMap<char, u64> = HashMap::new();
+    for chara in payload.chars() {
+        *memo.entry(chara).or_insert(0) += 1;
+    }
+    memo
 }
 
-println!("the original size is {} bits",size_original.to_string().red());
-println!("The size of The compressed One {} Bits ",size_compressed.to_string().green());
+fn main() {
+    let text = "Huffman coding is a lossless data compression algorithm...";
+    let freq = frequencies(text);
+    let mut min_heap: BinaryHeap<Tree> = BinaryHeap::new();
 
+    for (&ch, &freq) in &freq {
+        let current_tree = Tree {
+            score: freq,
+            label: Some(ch),
+            left: None,
+            right: None,
+        };
+        min_heap.push(current_tree);
+    }
 
+    while min_heap.len() >= 2 {
+        let node1 = min_heap.pop().unwrap();
+        let node2 = min_heap.pop().unwrap();
+        let new_node = Tree {
+            score: node1.score + node2.score,
+            label: None,  // Non-leaf nodes don't need a label
+            left: Some(Box::new(node1)),
+            right: Some(Box::new(node2)),
+        };
+        min_heap.push(new_node);
+    }
+
+    let head = min_heap.pop().unwrap();
+    let mut codes: HashMap<String, String> = HashMap::new();
+    find_character(&head, &mut codes, String::new());
+
+    let mut size_original = 0;
+    let mut size_compressed = 0;
+
+    for (label, code) in codes {
+        let ch = label.chars().next().unwrap();
+        size_compressed += (code.len() as u64) * freq[&ch];
+        size_original += freq[&ch] * 8;
+    }
+
+    println!("The original size is {} bits", size_original.to_string().red());
+    println!("The size of the compressed one is {} bits", size_compressed.to_string().green());
 }
-fn findCharacter(head : Tree ,codes :  &mut HashMap<String,String>,mut current_code :  String){
-if let None = head.left{
-codes.entry(head.label).or_insert(current_code );
-return;
-}else{
-let mut copy = current_code.clone(); 
 
-current_code.push('0');
-findCharacter(*head.left.unwrap() ,codes, current_code);
-copy.push('1');
-findCharacter(*head.right.unwrap() ,codes, copy);
+fn find_character(head: &Tree, codes: &mut HashMap<String, String>, current_code: String) {
+    if head.left.is_none() && head.right.is_none() {
+        if let Some(label) = head.label {
+            codes.insert(label.to_string(), current_code);
+        }
+        return;
+    }
+
+    if let Some(left) = &head.left {
+        find_character(left, codes, current_code.clone() + "0");
+    }
+    if let Some(right) = &head.right {
+        find_character(right, codes, current_code + "1");
+    }
 }
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
 
